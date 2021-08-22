9ea6bd5ddba4e2102576112a2420e593aec2c2bf
Abdurahman Alhifnawy <abdelrahman.alhifnawy.ext@orange.com>
2021-08-22 15:03:17 +0200

    Update TestBase.java

--- src/test/java/TestBase/TestBase.java
+++ src/test/java/TestBase/TestBase.java
@@ -3,6 +3,7 @@ package TestBase;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -22,6 +23,11 @@ public class TestBase {
         else if (browser.equalsIgnoreCase("Firefox")){
             WebDriverManager.firefoxdriver().setup();
             driver = new FirefoxDriver();
+            driver.manage().window().maximize();
+        }
+        else if (browser.equalsIgnoreCase("edge")){
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
             driver.manage().window().maximize();
         }
     }
